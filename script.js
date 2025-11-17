13570a51089b114800d412185c50262ea06e23c5
Alice Marques <alice.marques@even3.com>
2025-11-17 13:39:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+var app = angular.module("Pokemon", ['ngCookies']);
+
+app.controller("MainController", function ($scope, $http, $cookies) {
+
+  const savedPokemons = $cookies.getObject('pokemonsSalvos');
+  $scope.pokemons = Array.isArray(savedPokemons) ? savedPokemons : [];
+  $scope.displaySprite = '';
+  $scope.error = null;
+
+  var onPokemonComplete = function (response) {
+    $scope.pokemon = response.data;
+    $scope.displaySprite = $scope.pokemon.sprites.front_default;
+    $scope.error = null;
+
+    const name = $scope.pokemon.name;
+
+    if (!$scope.pokemons.includes(name)) {
+      $scope.pokemons.push(name);
+      $cookies.putObject('pokemonsSalvos', $scope.pokemons);
+    }
+
+    $scope.name = '';
+  };
+
+  var onError = function () {
+    $scope.error = "Não foi possível carregar os dados do Pokémon!";
+    $scope.pokemon = null;
+    $scope.displaySprite = '';
+  };
+
+  $http.get('translations.json').then(function (response) {
+    $scope.translations = response.data;
+  });
+
+  $scope.search = function (name) {
+    if (!name) return;
+
+    const pokemonName = name.toLowerCase();
+    const translated =
+      $scope.translations && $scope.translations[pokemonName]
+        ? $scope.translations[pokemonName].toLowerCase()
+        : pokemonName;
+
+    $http.get("https://pokeapi.co/api/v2/pokemon/" + translated)
+      .then(onPokemonComplete, onError);
+  };
+
+  $scope.changeSprite = function (type) {
+    if ($scope.pokemon && $scope.pokemon.sprites[type]) {
+      $scope.displaySprite = $scope.pokemon.sprites[type];
+    }
+  };
+
+  $scope.limparLista = function () {
+    $scope.pokemons = [];
+    $cookies.remove("pokemonsSalvos");
+  };
+
+});
